cf1ed941975fa5a1bdecc953fea319a4cab259f3
murugan22 <138664578+murugan22@users.noreply.github.com>
2023-10-10 15:35:54 +0530

    fixes

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
-import random
 
 app = Flask(__name__)
 
-# Define the directory where uploaded files will be stored
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-# Ensure the upload directory exists
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 class TLVParser:
@@ -18,7 +14,7 @@ class TLVParser:
     def parse_bytecode(self, bytecode):
         try:
             while len(bytecode) > 0:
-                tag = bytecode[0]
+                tag = bytecode[0:1]
                 bytecode = bytecode[1:]
 
                 if not bytecode:
@@ -26,16 +22,16 @@ class TLVParser:
                     self.error_code = "Incomplete data"
                     return
 
-                length_bytes = bytecode[:2]
+                length_bytes = bytecode[0:2]
                 bytecode = bytecode[2:]
-                length = ord(length_bytes[0]) * 256 + ord(length_bytes[1])
+                length = length_bytes[0] * 256 + length_bytes[1]
 
                 if len(bytecode) < length:
                     # Error: Incomplete data (insufficient value bytes)
                     self.error_code = "Incomplete data"
                     return
 
-                value = bytecode[:length]
+                value = bytecode[0:length]
                 bytecode = bytecode[length:]
 
                 # Process the tag, length, and value here
@@ -52,51 +48,10 @@ class TLVParser:
 
         except Exception as e:
             # Handle other exceptions and set an appropriate error code
-            self.error_code = "Other error"
+            self.error_code = f"Other error: {str(e)}"
 
     def get_error_code(self):
         return self.error_code
-
-
-class Fuzzer:
-    def __init__(self):
-        self.generated_values = []
-
-    def generate_values(self, error_codes):
-        generated_values = []
-
-        for code in error_codes:
-            if code == "Incomplete data":
-                # Generate value for incomplete data error
-                generated_values.append("default_value_for_incomplete_data")
-            elif code == "Missing size":
-                # Generate value for missing size error
-                generated_values.append("default_value_for_missing_size")
-            elif code == "Incorrect byte length":
-                # Generate value for incorrect byte length error
-                generated_values.append("default_value_for_incorrect_byte_length")
-            # Add more conditions for other error codes as needed
-
-        # Store the generated values in the instance variable
-        self.generated_values = generated_values
-
-    def get_generated_values(self):
-        return self.generated_values
-
-# Example usage
-fuzzer = Fuzzer()
-error_codes = ["Incomplete data", "Missing size", "Incorrect byte length"]
-
-# Generate values based on error codes using the Fuzzer
-fuzzer.generate_values(error_codes)
-generated_values = fuzzer.get_generated_values()
-
-# Print the generated values to the console
-print("Generated Values IN fuzzer:")
-for value in generated_values:
-    print(value)
-
-
 
 @app.route('/')
 def index():
@@ -104,36 +59,62 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    # Check if a file was uploaded
     if 'file' not in request.files:
         return redirect(request.url)
 
     file = request.files['file']
 
-    # If the user submits an empty form, handle it gracefully
     if file.filename == '':
         return redirect(request.url)
 
-    # Save the uploaded file to the upload directory
     if file:
         filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(filename)
 
-        # Process the uploaded file using TLVParser
         parser = TLVParser()
         with open(filename, 'rb') as bytecode_file:
             bytecode = bytecode_file.read()
             parser.parse_bytecode(bytecode)
-            error_codes = parser.get_error_codes()
+            error_code = parser.get_error_code()
 
-        # Generate values based on error codes using the Fuzzer
-        fuzzer = Fuzzer()
-        fuzzer.generate_values(error_codes)
-        generated_values = fuzzer.get_generated_values()
+            if error_code:
+                # Handle errors based on the error code
+                return f"Error: {error_code}"
 
-       
+            # If no errors, continue processing or generate values as needed
+
+            # Example: Generate values (modify this as needed)
+            generated_values = generate_values(bytecode)  # Replace with your value generation logic
+
+            # Print the generated values
+            print("Generated Values:")
+            for value in generated_values:
+                print(value)
 
     return 'File uploaded successfully!'
+
+# Example value generation function (replace with your logic)
+def generate_values(bytecode):
+    # Implement your value generation logic here
+    # For testing, you can return a list of sample values
+    generated_values = []
+    for code in error_codes:
+        if code == "Incomplete data":
+            # Generate value for incomplete data error
+            generated_values.append("default_value_for_incomplete_data")
+        elif code == "Missing size":
+            # Generate value for missing size error
+            generated_values.append("default_value_for_missing_size")
+        elif code == "Incorrect byte length":
+            # Generate value for incorrect byte length error
+            generated_values.append("default_value_for_incorrect_byte_length")
+        # Add more conditions for other error codes as needed
+
+    # Store the generated values in the instance variable
+    self.generated_values = generated_values
+    print(self.generated_values)
+    def get_generated_values(self):
+        return self.generated_values
 
 if __name__ == '__main__':
     app.run(debug=True)
